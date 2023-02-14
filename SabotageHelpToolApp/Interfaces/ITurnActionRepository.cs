d92dcb8ef6c6280fb117d629932aba3ce58cc952
HarryGu <harry.guapi@gmail.com>
2023-02-14 14:24:21 -0600

    Update .gitignore

---
+++ SabotageHelpToolApp/Interfaces/ITurnActionRepository.cs
@@ -0,0 +1,14 @@
+﻿using SabotageHelpToolApp.Models;
+
+namespace SabotageHelpToolApp.Interfaces
+{
+    public interface ITurnActionRepository
+    {
+        ICollection<TurnAction> GetTurnActions();
+
+        TurnAction GetTurnActionById(int id);
+
+        bool TurnActionExists(int id);
+
+    }
+}
---
+++ SabotageHelpToolApp/Repository/TurnActionRepository.cs
@@ -0,0 +1,31 @@
+﻿using SabotageHelpToolApp.Data;
+using SabotageHelpToolApp.Interfaces;
+using SabotageHelpToolApp.Models;
+
+namespace SabotageHelpToolApp.Repository
+{
+    public class TurnActionRepository : ITurnActionRepository
+    {
+        private readonly DataContext _context;
+
+        public TurnActionRepository(DataContext context)
+        {
+            this._context = context;
+        }
+
+        public TurnAction GetTurnActionById(int id)
+        {
+            return _context.TurnActions.Where(t => t.Id == id).FirstOrDefault();
+        }
+
+        public ICollection<TurnAction> GetTurnActions()
+        {
+            return _context.TurnActions.ToList();
+        }
+
+        public bool TurnActionExists(int id)
+        {
+            return _context.TurnActions.Any(t => t.Id == id);
+        }
+    }
+}
